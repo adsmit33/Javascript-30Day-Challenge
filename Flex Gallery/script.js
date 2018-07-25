068b53c358374abe7c99221aa700bc1dfa83a186
adsmit33 <austinsmith@nevada.unr.edu>
2018-07-25 08:22:53 -0700

    Flex Gallery

---
+++ Flex Gallery/script.js
@@ -0,0 +1,14 @@
+function openPanel(e) {
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+const panels = document.querySelectorAll(".panel");
+panels.forEach(panel => panel.addEventListener("click", openPanel));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
